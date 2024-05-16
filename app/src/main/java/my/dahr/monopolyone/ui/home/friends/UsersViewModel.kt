1a52b06376a683fc73cd8b4b6be43d1cc2730534
Bohdan <kalaczynskibo@gmail.com>
2024-05-16 20:09:14 +0300

    Was added some new code to AddFriendsFragment

--- app/src/main/java/my/dahr/monopolyone/ui/home/friends/UsersViewModel.kt
+++ app/src/main/java/my/dahr/monopolyone/ui/home/friends/UsersViewModel.kt
@@ -1,6 +1,7 @@
 package my.dahr.monopolyone.ui.home.friends
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import my.dahr.monopolyone.domain.repository.UsersRepository
@@ -9,6 +10,8 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import my.dahr.monopolyone.domain.models.friends.list.Friend
+import my.dahr.monopolyone.domain.models.users.Data
 
 @HiltViewModel
 class UsersViewModel @Inject constructor(
@@ -17,12 +20,14 @@ class UsersViewModel @Inject constructor(
 
     private val myCoroutineContext = SupervisorJob() + Dispatchers.IO
 
+    val usersResultLiveData = MutableLiveData<List<Data>>()
+
     fun getListOfUsers(userId: Any) {
         viewModelScope.launch(myCoroutineContext) {
             val response = usersRepository.getUsersList(
                 userId, setOf(1), "short"
             )
-            Log.d("users", response.toString())
+            usersResultLiveData.postValue(response)
         }
     }
 }
--- app/src/main/java/my/dahr/monopolyone/ui/home/friends/add/AddFriendsFragment.kt
+++ app/src/main/java/my/dahr/monopolyone/ui/home/friends/add/AddFriendsFragment.kt
@@ -6,10 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
+import my.dahr.monopolyone.R
 import my.dahr.monopolyone.databinding.FragmentAddFriendsBinding
+import my.dahr.monopolyone.domain.models.friends.list.Friend
+import my.dahr.monopolyone.domain.models.users.Data
+import my.dahr.monopolyone.ui.home.friends.FriendsFragment
 import my.dahr.monopolyone.ui.home.friends.FriendsViewModel
 import my.dahr.monopolyone.ui.home.friends.UsersViewModel
+import my.dahr.monopolyone.ui.home.friends.adapters.FriendsAdapter
+import my.dahr.monopolyone.ui.home.friends.user.UserFragment
 
 @AndroidEntryPoint
 class AddFriendsFragment : Fragment() {
@@ -27,14 +35,47 @@ class AddFriendsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setListeners()
         inputFriendNick()
+        initObservers()
     }
 
+    private fun setListeners() {
+        binding.ivBack.setOnClickListener {
+            val fragment = FriendsFragment()
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit()
+        }
+        binding.layoutUser.setOnClickListener{
 
-    private fun inputFriendNick(){
+        }
+    }
+
+    private fun initObservers() {
+        usersViewModel.usersResultLiveData.observe(viewLifecycleOwner) {
+            val avatar = it[0].avatar
+            val nick = it[0].nick
+            binding.apply {
+                layoutUser.visibility = View.VISIBLE
+                tvUserNick.text = nick
+
+            }
+            Glide.with(this)
+                .load(avatar)
+                .into(binding.ivUser)
+        }
+    }
+    private fun clickedOnUser(user: Data){
+
+    }
+
+    private fun inputFriendNick() {
         binding.btnAddFriend.setOnClickListener {
             val nick = binding.etFriendNick.text.toString()
             usersViewModel.getListOfUsers(nick)
+            binding.etFriendNick.text = null
+            binding.etFriendNick.text?.clear()
         }
     }
 
