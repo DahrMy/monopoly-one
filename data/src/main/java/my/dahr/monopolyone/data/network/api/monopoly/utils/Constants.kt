75aed5cea64471a3eab48c422141b39e51bceedb
Bohdan <kalaczynskibo@gmail.com>
2024-08-28 21:19:32 +0300

    deleted some packages from data module

--- data/src/main/java/my/dahr/monopolyone/data/network/api/monopoly/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package my.dahr.monopolyone.data.network.api.monopoly.utils
-
-const val BASE_URL = "https://monopoly-one.com/api/"
-const val MY_IP_BASE_URL = "https://api.myip.com/"
-
-const val SHARED_PREFERENCES = "shared_preferences"
-const val SESSION_KEY = "session"
-const val IP_KEY = "ip_address"
-
--- domain/src/main/java/my/dahr/monopolyone/domain/model/inventory/params/ItemParams.kt
+++ domain/src/main/java/my/dahr/monopolyone/domain/model/inventory/params/ItemParams.kt
@@ -1,7 +1,5 @@
 package my.dahr.monopolyone.domain.model.inventory.params
 
-import my.dahr.monopolyone.domain.model.ItemsParamsData
-
 data class ItemParams(
     val accessToken: String,
     val userId: Any,
